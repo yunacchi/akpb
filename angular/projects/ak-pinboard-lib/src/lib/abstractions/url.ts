e411d98a99ba6c120951332bfb1cff4a82b330ea
Yunacchi <26898968+yunacchi@users.noreply.github.com>
2020-05-18 08:59:32 +0200

    Use githack as CDN

--- angular/projects/ak-pinboard-lib/src/lib/abstractions/url.ts
+++ angular/projects/ak-pinboard-lib/src/lib/abstractions/url.ts
@@ -1,3 +1,4 @@
 // export const AkAssetsRootUrl = 'https://cdn.jsdelivr.net/gh/Aceship/AN-EN-Tags@master';
-export const AkAssetsRootUrl = 'https://raw.githubusercontent.com/Aceship/AN-EN-Tags/master';
+export const AkAssetsRootUrl = 'https://rawcdn.githack.com/Aceship/AN-EN-Tags/master';
 export const LocalAssetsRootUrl = 'assets';
+
